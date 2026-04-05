47e5b27d9ea616a7af06a8d0ada2758427afb6c7
Luke Ely <elyluk@amazon.com>
2026-04-05 16:14:51 +0100

    Fix header flash and match points skeleton width to actual component

--- frontend/app/components/landing-header.tsx
+++ frontend/app/components/landing-header.tsx
@@ -1,25 +1,12 @@
-'use client'
-
-import { useEffect, useState } from "react";
+import React from "react";
 import Link from "next/link";
-import { getCookie } from "cookies-next";
-import { TOKEN_COOKIE_KEY } from "@/app/api/api";
 import LandingHeaderLoginButton from "@/app/components/landing-header-login-button";
 
 export function Header(): React.JSX.Element {
-    const [loggedIn, setLoggedIn] = useState(false)
-
-    useEffect(() => {
-        setLoggedIn(!!getCookie(TOKEN_COOKIE_KEY))
-    }, [])
-
-    const buttonText = loggedIn ? "Go to App" : "Sign In"
-    const href = loggedIn ? "/app" : "/login"
-
     return <div className="z-50 w-full items-center justify-between text-sm flex max-w-3xl">
         <Link href="/">PREDICTABALL.LIVE</Link>
-        <Link href={href}>
-            <LandingHeaderLoginButton buttonText={buttonText}/>
+        <Link href="/login">
+            <LandingHeaderLoginButton buttonText="Sign In"/>
         </Link>
     </div>
 }
--- frontend/app/components/points/default-cards.tsx
+++ frontend/app/components/points/default-cards.tsx
@@ -2,25 +2,26 @@ import React from "react";
 
 export default function DefaultCards(): React.JSX.Element {
     return (
-        <div className="w-full flex mt-1 items-center text-center justify-around p-2">
-            <div className="w-1/5">
-                <div className="flex-row">
-                    <div className="font-bold text-white mb-1">Position</div>
-                    <div className="text-3xl rounded bg-gray-100 h-14 content-center">
-                        ...
+        <div className="w-full max-w-screen-lg mx-auto">
+            <div className="w-full flex mt-1 max-w-xl items-center text-center justify-around p-2 mx-auto">
+                <div className="w-1/5">
+                    <div className="flex-row">
+                        <div className="font-bold text-white mb-1">Position</div>
+                        <div className="text-3xl rounded bg-gray-100 h-14 content-center animate-pulse">
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div className="w-1/3">
-                <div className="flex-row">
-                    <div className="font-bold text-white mb-1">Points</div>
-                    <div className="text-5xl rounded bg-gray-100 h-24 content-center">0</div>
+                <div className="w-1/3">
+                    <div className="flex-row">
+                        <div className="font-bold text-white mb-1">Points</div>
+                        <div className="text-5xl rounded bg-gray-100 h-24 content-center animate-pulse"></div>
+                    </div>
                 </div>
-            </div>
-            <div className="w-1/5">
-                <div className="flex-row">
-                    <div className="font-bold text-white mb-1">Live</div>
-                    <div className="text-3xl rounded bg-gray-100 h-14 content-center">0</div>
+                <div className="w-1/5">
+                    <div className="flex-row">
+                        <div className="font-bold text-white mb-1">Live</div>
+                        <div className="text-3xl rounded bg-gray-100 h-14 content-center animate-pulse"></div>
+                    </div>
                 </div>
             </div>
         </div>
