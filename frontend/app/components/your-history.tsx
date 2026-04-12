efebb5533a201ca97dc73a5bde4dba732fbdcd8b
Luke Ely <elyluk@amazon.com>
2026-04-12 22:29:35 +0100

    Fix prediction history button blocking navigation

--- frontend/app/components/your-history.tsx
+++ frontend/app/components/your-history.tsx
@@ -1,20 +1,10 @@
-'use client'
-
 import { Button } from "@nextui-org/react"
 import { BUTTON_CLASS } from "@/app/util/css-classes"
-import { useState } from "react"
 
 export default function YourHistory(): React.JSX.Element {
-
-    const [isLoading, setIsLoading] = useState(false)
-
     return(
-            <Button
-                className={BUTTON_CLASS}
-                isLoading={isLoading}
-                onPress={() => setIsLoading(true)}
-            >
-                Your Prediction History
-            </Button>
+        <Button className={BUTTON_CLASS}>
+            Your Prediction History
+        </Button>
     )
-} 
+}
