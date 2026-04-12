27d16e727fa3c8617e811d23e42efa60c6d15e0c
Luke Ely <elyluk@amazon.com>
2026-04-12 22:27:41 +0100

    Fix sign in button blocking navigation

--- frontend/app/components/landing-header-login-button.tsx
+++ frontend/app/components/landing-header-login-button.tsx
@@ -1,20 +1,15 @@
 'use client'
 
 import {Button} from "@nextui-org/react";
-import React, {useState} from "react";
+import React from "react";
 
 interface LandingHeaderLoginButtonProps {
     buttonText?: string
 }
 
 const LandingHeaderLoginButton = (props: LandingHeaderLoginButtonProps) => {
-
-    const [isLoading, setIsLoading] = useState(false)
-
     return (
         <Button
-            onPress={() => setIsLoading(true)}
-            isLoading={isLoading}
             className={"bg-gradient-to-tr from-blue-600 to-green-300 shadow-lg text-white"}
         >
             {props.buttonText !== undefined ? props.buttonText : "Sign In"}
